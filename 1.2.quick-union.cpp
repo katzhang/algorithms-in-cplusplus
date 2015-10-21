f1a63b3a3a713cfdd95d820019d0a02547cfa72b
katzhang <arcadychang@gmail.com>
2015-10-20 22:00:07 -0400

    added quick union and renamed files

---
+++ 1.2.quick-union.cpp
@@ -0,0 +1,28 @@
+/*
+author: Robert Sedgewick
+PROG: Quick-union solution to connectivity problem
+*/
+
+#include <iostream>
+
+using namespace std;
+
+static const int N = 10000;
+
+int main() {
+	int i, j, p, q, id[N];
+
+	for (i = 0; i < N; i++) {
+		id[i] = i;
+	}
+
+	while (cin >> p >> q) {
+		for (i = p; i != id[i]; i = id[i]);
+		for (j = q; j != id[j]; j = id[j]);
+		if (i == j) {
+			continue;
+		}
+		id[i] == j;
+		cout << " " << p << " " << q << endl;
+	}
+}
