76cddeab254a18fdca889db0fef109148c4f9cc6
katzhang <arcadychang@gmail.com>
2015-11-05 22:26:44 -0500

    added list interface

---
+++ 3.12.list-processing-interface.h
@@ -0,0 +1,26 @@
+/*
+author: Robert Sedgewick
+PROG: List-processing interface
+*/
+
+#include <iostream>
+
+using namespace std;
+
+typedef int Item;
+struct node {
+	Item item;
+	node* next;
+};
+typedef node* link;
+typedef link Node;
+
+void construct(int);
+Node newNode(int);
+void deleteNode(Node);
+void insert(Node, Node);
+Node remove(Node);
+Node next(Node);
+Item item(Node);
+
+
