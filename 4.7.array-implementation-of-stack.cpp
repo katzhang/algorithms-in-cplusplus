e8e0df3d740b7485f65c131a2804e346a23c8fc4
katzhang <arcadychang@gmail.com>
2015-11-11 22:38:48 -0500

    added two types of implementations of stacks

---
+++ 4.7.array-implementation-of-stack.cpp
@@ -0,0 +1,27 @@
+/*
+author: Robert Sedgewick
+PROG: Array implementation of a pushdown stack
+*/
+
+#include <iostream>
+
+template <class Item>
+class STACK {
+private:
+	Item* s;
+	int N;
+public:
+	STACK(int maxN) {
+		s = new Item[maxN];
+		N = 0;
+	}
+	int empty() {
+		return N == 0;
+	}
+	void push(Item item) {
+		s[N++] = item;
+	}
+	Item pop() {
+		return s[--N];
+	}
+}
---
+++ 4.8.linked-list-implementation-of-stack.cpp
@@ -0,0 +1,38 @@
+/*
+author: Robert Sedgewick
+PROG: Linked-list implementation of a pushdown stack
+*/
+
+#include <iostream>
+
+template <class Item>
+class STACK {
+private:
+	struct node {
+		Item item;
+		node* next;
+		node(Item x, node* t) {
+			item = x;
+			next = t;
+		}
+		typedef node* nodeLink;
+		nodeLink head;
+	};
+public:
+	STACK(int) {
+		head = 0;
+	}
+	int empty() {
+		return head == 0;
+	}
+	void push(Item x) {
+		head = new node(x, head);
+	}
+	Item pop() {
+		Item v = head->item;
+		nodeLink second = head->next;
+		delete head;
+		head = second;
+		return v;
+	}
+}
