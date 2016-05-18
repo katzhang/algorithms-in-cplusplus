9105a527f5b7549e24dea5f66eb8e357cfd78d17
katzhang <arcadychang@gmail.com>
2016-05-17 23:23:14 -0400

    added reverse digraph

---
+++ 19.1.reverse-digraph.cpp
@@ -0,0 +1,21 @@
+/*
+author: Robert Sedgewick
+PROG: Reversing a digraph
+*/
+
+#include <iostream>
+
+using namespace std;
+
+template<class inGraph, class outGraph> void reverse(const inGraph &G, outGraph &R) {
+	for (int v = 0; v < G.V(); v++) {
+		typename inGraph::adjIterator A(G, v);
+		for (int w = A.beg(); !A.end(); w = A.nxt()) {
+			R.insert(Edge(w, v));
+		}
+	}
+}
+
+
+
+
