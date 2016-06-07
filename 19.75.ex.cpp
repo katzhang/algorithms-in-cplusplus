ca6365aa5b486fb226374ea33c28cb9bf7b56f61
katzhang <arcadychang@gmail.com>
2016-06-06 22:01:58 -0400

    added topological sorting

---
+++ 19.75.ex.cpp
@@ -0,0 +1,54 @@
+/*
+author: Kat Zhang
+PROG: Implement a DFS class for use by clients for verifying that a DAG has no cycles
+*/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template <class Graph> class DFS {
+	const Graph &G;
+	int depth, cnt, cntP;
+	int backCnt;
+	vector<int> pre, post;
+	void show(char *s, Edge e) {
+		for (int i = 0; i < depth; i++) {
+			cout << " ";
+		}
+		cout << e.v << "-" << e.w << s << endl;
+	}
+	void dfsR(Edge e) {
+		int w = e.w;
+		show(" tree", e);
+		pre[w] = cnt++;
+		depth++;
+		typename Graph::adjIterator A(G, w);
+		for (int t = A.beg(); !A.end(); t = A.nxt()) {
+			Edge x(w, t);
+			if (pre[t] == -1) {
+				dfsR(x);
+			} else if (post[t] == -1) {
+				backCnt++;
+			}
+		}
+		post[w] = cntP++;
+		depth--;
+	}
+public:
+	DFS(const Graph &G) : G(G), cnt(0), cntP(0), backCnt(0), pre(G.V(), -1), post(G.V(), -1) {
+		for (int v = 0; v < G.V(); v++) {
+			if (pre[v] == -1) {
+				dfsR(Edge(v, v));
+			}
+		}
+	}
+	bool isDAG() {
+		return backCnt == 0;
+	}
+};
+
+
+
+
